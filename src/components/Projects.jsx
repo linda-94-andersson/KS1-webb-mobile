1ec4febc3a15bf789dc6991fcf7951ba330b9781
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-18 18:40:29 +0200

    Render out projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useProject } from "../context/ProjectContext";
 
 function Projects() {
+  const { project } = useProject();
+
   return (
     <>
-      <div>
-        <h2>Project name 1</h2>
-      </div>
-      <div>
-        <h2>Project name 2</h2>
-      </div>
+      {project ? (
+        project.map((p) => (
+          <div key={p.id}>
+            <h2>{p.name}</h2>
+          </div>
+        ))
+      ) : (
+        <div>
+          <h2>No projects found</h2>
+        </div>
+      )}
       <button>Add new project</button>
     </>
   );

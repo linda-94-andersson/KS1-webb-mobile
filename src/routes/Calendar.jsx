e6f4af46f24229afe17c9ae00820aee9bb776dd9
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-19 17:01:09 +0200

    trying a setup for date range

--- src/routes/Calendar.jsx
+++ src/routes/Calendar.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import DateRangePicker from "@wojtekmaj/react-daterange-picker/dist/entry.nostyle";
+import moment from "moment";
 import { useUser } from "../context/UserContext";
 
 function Calendar() {
+  const [value, onChange] = useState([new Date(), new Date()]);
+
   const { userValue } = useUser();
+
+  const renderDayTaks = () => {
+    console.log(value, " vad är value?");
+    return (
+      <div>
+        <h2>
+          {userValue.user ? (
+            userValue.user.map((u) => (
+              <p key={u.id}>
+                <span>{u.name}</span>
+              </p>
+            ))
+          ) : (
+            <span>No users found</span>
+          )}
+        </h2>
+        <h3>task name</h3>
+        <h4>timer</h4>
+      </div>
+    );
+  };
 
   return (
     <>
@@ -10,25 +35,14 @@ function Calendar() {
         <h1>Calendar</h1>
       </header>
       <section>
-        <select>
-          <option>Dagens datum</option>
-          <option>Datum</option>
-        </select>
-        <div>
-          <h2>
-            {userValue.user ? (
-              userValue.user.map((u) => (
-                <p key={u.id}>
-                  <span>{u.name}</span>
-                </p>
-              ))
-            ) : (
-              <span>No users found</span>
-            )}
-          </h2>
-          <h3>task name</h3>
-          <h4>timer</h4>
-        </div>
+        <DateRangePicker
+          onChange={onChange}
+          value={value}
+          disableCalendar={true}
+          required={true}
+          showLeadingZeros={true}
+        />
+        {renderDayTaks()}
       </section>
     </>
   );

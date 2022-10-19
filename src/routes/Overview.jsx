297e191e5abf25b4440e83867e01e62c583d1dfc
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-19 12:04:42 +0200

    Fixed seprate page view

--- src/routes/Overview.jsx
+++ src/routes/Overview.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Projects from "../components/Projects";
 import Tasks from "../components/Tasks";
 import User from "../components/User";
 
 function Overview() {
+  const [isSelUser, setIsSelUser] = useState(true);
+  const [isSelPro, setIsSelPro] = useState(false);
+  const [isSelTas, setIsSelTas] = useState(false);
+
+  const handleUsers = () => {
+    setIsSelUser(true);
+    setIsSelPro(false);
+    setIsSelTas(false);
+  };
+
+  const handleProjects = () => {
+    setIsSelPro(true);
+    setIsSelTas(false);
+    setIsSelUser(false);
+  };
+
+  const handleTasks = () => {
+    setIsSelTas(true);
+    setIsSelUser(false);
+    setIsSelPro(false);
+  };
+
   return (
     <>
       <header>
@@ -12,14 +34,20 @@ function Overview() {
       <section>
         <nav>
           <ul>
-            <li>Users</li>
-            <li>Projects</li>
-            <li>Tasks</li>
+            <li>
+              <button onClick={handleUsers}>Users</button>
+            </li>
+            <li>
+              <button onClick={handleProjects}>Projects</button>
+            </li>
+            <li>
+              <button onClick={handleTasks}>Tasks</button>
+            </li>
           </ul>
         </nav>
-        <User />
-        <Projects />
-        <Tasks />
+        {isSelUser && <User setIsSelUser={setIsSelUser} />}
+        {isSelPro && <Projects setIsSelPro={setIsSelPro} />}
+        {isSelTas && <Tasks setIsSelTas={setIsSelTas} />}
       </section>
     </>
   );

69fee77826a86473ad9384039578db9a2d9c2962
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-21 21:04:22 +0200

    Removed index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-// import "./index.css";
 import Root from "./routes/Root";
 import ErrorPage from "./Error.page";
 import Calendar from "./routes/Calendar";

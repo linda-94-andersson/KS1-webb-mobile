0edf0319ce96aba67ee0371f482d4c23ea3b4932
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-21 19:46:00 +0200

    Styled calendar task renders

--- src/routes/Calendar.jsx
+++ src/routes/Calendar.jsx
@@ -3,6 +3,9 @@ import DateRangePicker from "@wojtekmaj/react-daterange-picker/dist/entry.nostyl
 import { useUser } from "../context/UserContext";
 import { useTask } from "../context/TaskContext";
 import { useProject } from "../context/ProjectContext";
+import { Center, Heading, Container, Box, Divider } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/icons";
+import { MdOutlineColorLens } from "react-icons/md";
 
 function Calendar() {
   const [value, onChange] = useState([new Date(), new Date()]);
@@ -11,51 +14,74 @@ function Calendar() {
   const { projectValue } = useProject();
   const { taskValue } = useTask();
 
-  const renderDayTaks = () => {
-    // console.log(value, " vad är value?");
+  // console.log(value, " vad är value?");
 
+  const renderDayTaks = () => {
     return (
-      <div>
+      <Container>
         {taskValue.task ? (
           taskValue.task.map((t) => (
-            <div key={t.id}>
+            <Box key={t.id}>
               {projectValue.project
                 .filter((p) => p.id === t.projectId)
                 .map((i) => (
-                  <div key={i.id}>
+                  <Box key={i.id}>
+                    <Icon
+                      as={MdOutlineColorLens}
+                      w={25}
+                      h={25}
+                      key={i.id}
+                      style={{
+                        backgroundColor: i.color,
+                      }}
+                    ></Icon>
                     {userValue.user
                       .filter((u) => u.id === i.userId)
                       .map((j) => (
-                        <h3 key={j.id}>{j.name}</h3>
+                        <Heading as="h3" size="lg" key={j.id}>
+                          {j.name}
+                        </Heading>
                       ))}
-                  </div>
+                  </Box>
                 ))}
-              <h2>{t.name}</h2>
-              <h4>timer</h4>
-            </div>
+              <Heading as="h2" size="2xl">
+                {t.name}
+              </Heading>
+              <Heading as="h4" size="md">
+                timer
+              </Heading>
+              <Divider />
+              <br />
+            </Box>
           ))
         ) : (
-          <span>No tasks found</span>
+          <Heading>No tasks found</Heading>
         )}
-      </div>
+      </Container>
     );
   };
 
   return (
     <>
-      <header>
-        <h1>Calendar</h1>
+      <header style={{ paddingBottom: 50 }}>
+        <Center>
+          <Heading as="h1" size="3xl">
+            Calendar
+          </Heading>
+        </Center>
       </header>
-      <section>
-        <DateRangePicker
-          onChange={onChange}
-          value={value}
-          disableCalendar={true}
-          required={true}
-          showLeadingZeros={true}
-        />
+      <Container>
+        <Center>
+          <DateRangePicker
+            onChange={onChange}
+            value={value}
+            disableCalendar={true}
+            required={true}
+            showLeadingZeros={true}
+          />
+        </Center>
         {renderDayTaks()}
-      </section>
+      </Container>
     </>
   );
 }

cfa635160c0ef16d2956383754e4b75eb4a9e4e4
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-21 17:25:58 +0200

    Center footer

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -4,7 +4,13 @@ import { ProjectProvider } from "../context/ProjectContext";
 import { TaskProvider } from "../context/TaskContext";
 import { TimeLogProvider } from "../context/TimeLogContext";
 import { UserProvider } from "../context/UserContext";
-import { Link as ReachLink, Tabs, TabList, Tab } from "@chakra-ui/react";
+import {
+  Center,
+  Link as ReachLink,
+  Tabs,
+  TabList,
+  Tab,
+} from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/icons";
 import { MdTimer } from "react-icons/md";
 import { AiOutlineCalendar } from "react-icons/ai";
@@ -17,27 +23,29 @@ function Root() {
         <ProjectProvider>
           <TaskProvider>
             <TimeLogProvider>
-              <footer style={{ position: "fixed", bottom: 0 }}>
-                <Tabs>
-                  <TabList>
-                    <Tab>
-                      <Link as={ReachLink} to={`/`}>
-                        <Icon as={MdTimer} w={75} h={75} />
-                      </Link>
-                    </Tab>
-                    <Tab>
-                      <Link as={ReachLink} to={`calendar`}>
-                        <Icon as={AiOutlineCalendar} w={75} h={75} />
-                      </Link>
-                    </Tab>
-                    <Tab>
-                      <Link as={ReachLink} to={`overview`}>
-                        <Icon as={BiTask} w={75} h={75} />
-                      </Link>
-                    </Tab>
-                  </TabList>
-                </Tabs>
-              </footer>
+              <Center>
+                <footer style={{ position: "fixed", bottom: 0 }}>
+                  <Tabs>
+                    <TabList>
+                      <Tab>
+                        <Link as={ReachLink} to={`/`}>
+                          <Icon as={MdTimer} w={75} h={75} />
+                        </Link>
+                      </Tab>
+                      <Tab>
+                        <Link as={ReachLink} to={`calendar`}>
+                          <Icon as={AiOutlineCalendar} w={75} h={75} />
+                        </Link>
+                      </Tab>
+                      <Tab>
+                        <Link as={ReachLink} to={`overview`}>
+                          <Icon as={BiTask} w={75} h={75} />
+                        </Link>
+                      </Tab>
+                    </TabList>
+                  </Tabs>
+                </footer>
+              </Center>
               <Outlet />
             </TimeLogProvider>
           </TaskProvider>

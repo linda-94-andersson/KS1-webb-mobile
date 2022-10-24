aa031adccd45827d068cf963ca9e47e4d86ac0c5
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-24 17:03:30 +0200

    Updated footer styling

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -4,13 +4,7 @@ import { ProjectProvider } from "../context/ProjectContext";
 import { TaskProvider } from "../context/TaskContext";
 import { TimeLogProvider } from "../context/TimeLogContext";
 import { UserProvider } from "../context/UserContext";
-import {
-  Center,
-  Link as ReachLink,
-  Tabs,
-  TabList,
-  Tab,
-} from "@chakra-ui/react";
+import { Link as ReachLink, Tabs, TabList, Tab } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/icons";
 import { MdTimer } from "react-icons/md";
 import { AiOutlineCalendar } from "react-icons/ai";
@@ -24,37 +18,36 @@ function Root() {
           <TaskProvider>
             <TimeLogProvider>
               <Outlet />
-              <Center>
-                <footer
-                  style={{
-                    position: "fixed",
-                    bottom: 0,
-                    backgroundColor: "white",
-                    zIndex: 2,
-                    padding: "5px",
-                  }}
-                >
-                  <Tabs>
-                    <TabList>
-                      <Tab>
-                        <Link as={ReachLink} to={`/`}>
-                          <Icon as={MdTimer} w={75} h={75} />
-                        </Link>
-                      </Tab>
-                      <Tab>
-                        <Link as={ReachLink} to={`calendar`}>
-                          <Icon as={AiOutlineCalendar} w={75} h={75} />
-                        </Link>
-                      </Tab>
-                      <Tab>
-                        <Link as={ReachLink} to={`overview`}>
-                          <Icon as={BiTask} w={75} h={75} />
-                        </Link>
-                      </Tab>
-                    </TabList>
-                  </Tabs>
-                </footer>
-              </Center>
+              <footer
+                style={{
+                  position: "fixed",
+                  bottom: 0,
+                  paddingLeft: 25,
+                  width: "100%",
+                  backgroundColor: "white",
+                  zIndex: 2,
+                }}
+              >
+                <Tabs>
+                  <TabList>
+                    <Tab>
+                      <Link as={ReachLink} to={`/`}>
+                        <Icon as={MdTimer} w={75} h={75} />
+                      </Link>
+                    </Tab>
+                    <Tab>
+                      <Link as={ReachLink} to={`calendar`}>
+                        <Icon as={AiOutlineCalendar} w={75} h={75} />
+                      </Link>
+                    </Tab>
+                    <Tab>
+                      <Link as={ReachLink} to={`overview`}>
+                        <Icon as={BiTask} w={75} h={75} />
+                      </Link>
+                    </Tab>
+                  </TabList>
+                </Tabs>
+              </footer>
             </TimeLogProvider>
           </TaskProvider>
         </ProjectProvider>
